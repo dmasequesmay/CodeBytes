c60d3c7a67a630b6a316ba232ea591d7893e220c
Arvick <macalaladjustin@gmail.com>
2024-12-19 16:12:20 -0800

    change container setup to include more elements

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,5 +1,15 @@
-console.log("test");
-console.log("end");
-while (true){
-    
-}
+const express = require('express');
+// const firebase = require('./firebaseConfig');
+const elasticSearch = require('./elasticSearchConfig');
+// const { createLeaderboard } = require('./keepTheScoreConfig');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on http://localhost:5000');
+});
---
+++ backend/app/keepTheScoreConfig.js
@@ -0,0 +1,13 @@
+// const axios = require('axios');
+// const baseURL = 'https://api.keepthescore.co';
+// const apiKey = process.env.KEEP_THE_SCORE_API_KEY;
+
+// const createLeaderboard = async (title) => {
+//   const response = await axios.post(`${baseURL}/leaderboards/`, {
+//     title,
+//     key: apiKey,
+//   });
+//   return response.data;
+// };
+// module.exports = { createLeaderboard };
+
