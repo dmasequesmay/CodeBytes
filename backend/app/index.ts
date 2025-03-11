17be139076dfb1a775cffbff163eed16c414f07b
KennethAlbarillo <kalbaril@uci.edu>
2025-03-11 00:03:32 -0700

    Delete other two endpoints. Changed INSERT INTO statement. Changed the data type for difficulty to utilize enum defined in types/enums.ts.

--- backend/app/index.ts
+++ backend/app/index.ts
@@ -42,34 +42,16 @@ app.post("/add-user ", (req, res) => {
 });
 
 /*TODO: write the backend endpoint for adding to the class table */
-app.post("/add-class ", (req, res) => {
-  const { className } : {
-    className:string
-  } = req.body;
-
-  dpool.query(
-    'INSERT INTO classes (className)',
-    [className]
-  )
-  .then(() => {
-    res.status(200).send("success!");
-  })
-  .catch((error) => {
-    console.error('Error inserting data:', error);
-    res.status(500).send('Internal Server Error');
-  });
-});
-
 
 /*TODO: write the backend endpoint for adding to the lessons table */
 app.post("/add-lesson ", (req, res) => {
   const { lessonName, difficulty } : {
     lessonName:string,
-    difficulty:string
+    difficulty:lessonDifficulty,
   } = req.body;
 
   dpool.query(
-    'INSERT INTO lessons (lessonName, difficulty)',
+    'INSERT INTO lessons (lessonName, difficulty) VALUES ($1, $2)',
     [lessonName, difficulty]
   )
   .then(() => {
@@ -83,28 +65,3 @@ app.post("/add-lesson ", (req, res) => {
 
 
 /*TODO: write the backend endpoint for adding to the badges table */
-app.post("/add-badge ", (req, res) => {
-  const { badgeName, badgeDesc, requirement, badgeImageSrc } : {
-    badgeName:string,
-    badgeDesc:string,
-    requirement:string,
-    badgeImageSrc:string
-  } = req.body;
-
-  dpool.query(
-    'INSERT INTO badges(badgeName, badgeDesc, requirement, badgeImageSrc)',
-    [badgeName, badgeDesc, requirement, badgeImageSrc]
-  )
-  .then(() => {
-    res.status(200).send("success!");
-  })
-  .catch((error) => {
-    console.error('Error inserting data', error);
-    res.status(500).send('Internal Server Error');
-  });
-});
-
-
-app.listen(5000, () => {
-  console.log('Server is running on http://localhost:5000');
-});
