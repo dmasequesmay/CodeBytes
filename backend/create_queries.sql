0f7f589aa4ee02e28222014d3f73ad16f6f637ac
jerrick-dev <jerricka@uci.edu>
2025-03-11 03:05:45 -0700

    moved under correct section

--- backend/create_queries.sql
+++ backend/create_queries.sql
@@ -62,15 +62,7 @@ CREATE TABLE IF NOT EXISTS UserOwnedBadges (
 -- TODO: write CREATE TABLE statement for user_progress
 
 -- TODO: write CREATE TABLE statement for lessons
-CREATE TABLE IF NOT EXISTS user_lesson_progress(
-    userId INT NOT NULL,
-    lessonId INT NOT NULL,
-    dateStart DATE NOT NULL,
-    dateFinished DATE,
-    PRIMARY KEY (userId, lessonId),
-    FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE,
-    FOREIGN KEY (lessonId) REFERENCES Lessons(id) ON DELETE CASCADE
-)
+
 -- TODO: write CREATE TABLE statement for classes
 
 -- TODO: write CREATE TABLE statment for class_membership
@@ -84,5 +76,13 @@ CREATE TABLE IF NOT EXISTS class_membership(
 );
 
 -- TODO: write CREATE TABLE statement for user_lesson_progress
-
+CREATE TABLE IF NOT EXISTS user_lesson_progress(
+    userId INT NOT NULL,
+    lessonId INT NOT NULL,
+    dateStart DATE NOT NULL,
+    dateFinished DATE,
+    PRIMARY KEY (userId, lessonId),
+    FOREIGN KEY (userId) REFERENCES Users(id) ON DELETE CASCADE,
+    FOREIGN KEY (lessonId) REFERENCES Lessons(id) ON DELETE CASCADE
+)
 
