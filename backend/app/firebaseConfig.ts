4eb31e1437c703177a71c722a9fdcd58c4a19004
Arvick <macalaladjustin@gmail.com>
2025-04-13 22:31:24 -0700

    added auth to backend firebase config

--- backend/app/firebaseConfig.ts
+++ backend/app/firebaseConfig.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-
+import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -10,5 +10,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { app, db };
+export { app, db, auth };
