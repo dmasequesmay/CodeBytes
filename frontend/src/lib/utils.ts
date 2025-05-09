bdaba80d0baced8494caf0fb0ac62d2655ebc9d5
amlimjoc <amlimjoc@uci.edu>
2025-05-08 21:26:35 -0700

    updated utils.ts to manage user data from localstorage

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,25 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Combines Tailwind classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// Save user data to localStorage
+export function saveUserToLocalStorage(user: any) {
+  if (user) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+}
+
+// Get user data from localStorage
+export function getUserFromLocalStorage(): any {
+  const user = localStorage.getItem('user');
+  return user ? JSON.parse(user) : null;
+}
+
+// Remove user data from localStorage
+export function removeUserFromLocalStorage() {
+  localStorage.removeItem('user');
 }
